aa41967e5f5d20f44c2bbb05d925a1402e0e6e8a
201511035 <mira0809@naver.com>
2017-05-17 23:22:31 +0900

    #784 메인화면 ui구현

---
+++ src/main/java/se/smu/SUBJECT_MANAGEMENT.java
@@ -0,0 +1,44 @@
+package se.smu;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class SUBJECT_MANAGEMENT extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SUBJECT_MANAGEMENT frame = new SUBJECT_MANAGEMENT();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public SUBJECT_MANAGEMENT() {
+		setTitle("수강 과목");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 585, 556);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
