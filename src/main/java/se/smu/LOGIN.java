d9a6457b1b3ca2a59a923c37e9d6119ce59829df
201511035 <mira0809@naver.com>
2017-05-21 05:26:33 +0900

    LOGIN UI

--- src/main/java/se/smu/LOGIN.java
+++ src/main/java/se/smu/LOGIN.java
@@ -5,43 +5,42 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
 import javax.swing.ImageIcon;
 
 public class LOGIN extends JFrame {
 
 	private JPanel contentPane;
 
-	
+
 	public static void main(String[] args) {
-		 SwingUtilities.invokeLater(new Runnable() {
+		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				new LOGIN();	
+				try {
+					LOGIN frame = new LOGIN();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
 	}
-	
 
-	
+
 	public LOGIN() {
-		LOGIN frame = new LOGIN();
 		setTitle("로그인");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 511, 628);
+		setBounds(100, 100, 529, 630);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		ImageIcon icon = new ImageIcon(LOGIN.class.getResource("https://github.com/SE-admin/SE2017_2_mixharmony.git\\학교명.jpg"));
-		JLabel label = new JLabel(new ImageIcon(LOGIN.class.getResource("/images/학교명.jpg")));	
-		label.setBounds(17, 15, 51, 46);
-		contentPane.add(label);
-		//contentPane.pack();
-		//contentPane.setLocation(true);
-		contentPane.setVisible(true);
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(LOGIN.class.getResource("/images/학교명.jpg")));
+		lblNewLabel.setBounds(17, 27, 52, 41);
+		contentPane.add(lblNewLabel);
 	}
+
 }
