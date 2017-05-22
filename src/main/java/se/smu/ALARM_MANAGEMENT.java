ad9c3993d844513439e1117786da2715b2825a63
201511035 <mira0809@naver.com>
2017-05-22 22:06:10 +0900

    ALARM_MANAGEMENT.java UI

--- src/main/java/se/smu/ALARM_MANAGEMENT.java
+++ src/main/java/se/smu/ALARM_MANAGEMENT.java
@@ -37,26 +37,27 @@ public class ALARM_MANAGEMENT extends JFrame {
 	public ALARM_MANAGEMENT() {
 		setTitle("알람 게시판");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 477, 585);
+		setBounds(100, 100, 477, 629);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(ALARM_MANAGEMENT.class.getResource("/images/alarm-clipart-Alarm-Clock-15-iloveimg-resized.png")));
-		lblNewLabel.setBounds(40, 15, 375, 218);
+		lblNewLabel.setIcon(new ImageIcon(ALARM_MANAGEMENT.class.getResource("/image/alarm-clipart-Alarm-Clock-15-iloveimg-resized.png")));
+		lblNewLabel.setBounds(35, 15, 375, 269);
 		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(17, 299, 393, 259);
+		contentPane.add(textField);
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setColumns(10);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(17, 253, 421, 261);
+		scrollPane.setBounds(17, 297, 421, 261);
 		contentPane.add(scrollPane);
-		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		scrollPane.setViewportView(textField);
-		textField.setColumns(10);
 	}
 }
