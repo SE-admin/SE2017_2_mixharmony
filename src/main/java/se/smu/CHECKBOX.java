1f2a9804e6648be1c91cbca2214ecfd8ce79c177
201511035 <mira0809@naver.com>
2017-05-23 00:52:55 +0900

    #784 CHECKBOX.java UI FINAL

--- src/main/java/se/smu/CHECKBOX.java
+++ src/main/java/se/smu/CHECKBOX.java
@@ -1,6 +1,6 @@
 package se.smu;
 
-import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -11,7 +11,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.swing.JLabel;
+import javax.swing.JButton;
+
 
 public class CHECKBOX extends JFrame {
 
@@ -40,7 +41,7 @@ public class CHECKBOX extends JFrame {
 		});
 		setTitle("확인");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 574, 237);
+		setBounds(100, 100, 574, 292);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
@@ -49,13 +50,19 @@ public class CHECKBOX extends JFrame {
 		JTextArea textArea = new JTextArea();
 		textArea.setWrapStyleWord(true);
 		textArea.setToolTipText("");
-		textArea.setText("                  * 확 인 *\r\n\r\n모든 항목이 정상적으로 등록되었습니다.");
+		textArea.setText("                                           * 확 인 *\r\n\r\n                     모든 항목이 정상적으로 등록되었습니다.");
 		textArea.setLineWrap(true);
 		textArea.setForeground(Color.BLACK);
 		textArea.setFont(textArea.getFont().deriveFont(textArea.getFont().getStyle() | Font.BOLD, textArea.getFont().getSize() + 6f));
 		textArea.setBackground(Color.WHITE);
 		textArea.setBounds(17, 15, 525, 146);
 		contentPane.add(textArea);
+		
+		JButton button = new JButton("확인");
+		button.setForeground(Color.WHITE);
+		button.setFont(button.getFont().deriveFont(button.getFont().getStyle() | Font.BOLD, button.getFont().getSize() + 2f));
+		button.setBackground(new Color(0, 0, 128));
+		button.setBounds(399, 188, 136, 33);
+		contentPane.add(button);
 	}
-
 }
