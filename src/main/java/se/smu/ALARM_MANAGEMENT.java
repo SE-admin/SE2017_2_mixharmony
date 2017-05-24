50f9b912b9b2f429268caa9914c9347833109815
201511035 <mira0809@naver.com>
2017-05-25 01:43:35 +0900

    #758 ALARM_MANAGEMENT.java ui push after feedback

--- src/main/java/se/smu/ALARM_MANAGEMENT.java
+++ src/main/java/se/smu/ALARM_MANAGEMENT.java
@@ -11,11 +11,16 @@ import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import java.awt.Font;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
 
 public class ALARM_MANAGEMENT extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField Alarm_Tf;
+	private JTable Alarm_Anounce_Tb;
 
 
 	public static void main(String[] args) {
@@ -35,7 +40,7 @@ public class ALARM_MANAGEMENT extends JFrame {
 	public ALARM_MANAGEMENT() {
 		setTitle("알람 게시판");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 477, 629);
+		setBounds(100, 100, 593, 686);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
@@ -43,18 +48,34 @@ public class ALARM_MANAGEMENT extends JFrame {
 		
 		JLabel Alarm_Img = new JLabel("");
 		Alarm_Img.setIcon(new ImageIcon(ALARM_MANAGEMENT.class.getResource("/image/alarm-clipart-Alarm-Clock-15-iloveimg-resized.png")));
-		Alarm_Img.setBounds(35, 15, 387, 269);
+		Alarm_Img.setBounds(101, 15, 409, 284);
 		contentPane.add(Alarm_Img);
 		
 		JScrollPane Alarm_Scroll = new JScrollPane();
 		Alarm_Scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		Alarm_Scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		Alarm_Scroll.setBounds(17, 297, 421, 261);
+		Alarm_Scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		Alarm_Scroll.setBounds(17, 314, 543, 294);
 		contentPane.add(Alarm_Scroll);
 		
-		Alarm_Tf = new JTextField();
-		Alarm_Tf.setHorizontalAlignment(SwingConstants.CENTER);
-		Alarm_Scroll.setViewportView(Alarm_Tf);
-		Alarm_Tf.setColumns(10);
-	}
+		Alarm_Anounce_Tb = new JTable();
+		Alarm_Anounce_Tb.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"\uACFC\uBAA9\uBA85", "todo\uBA85", "\uB9C8\uAC10 \uAE30\uD55C", "\uC911\uC694\uB3C4"
+			}
+		));
+		Alarm_Anounce_Tb.getColumnModel().getColumn(0).setPreferredWidth(110);
+		Alarm_Anounce_Tb.getColumnModel().getColumn(1).setPreferredWidth(99);
+		Alarm_Anounce_Tb.getColumnModel().getColumn(2).setPreferredWidth(104);
+		Alarm_Anounce_Tb.getColumnModel().getColumn(3).setPreferredWidth(82);
+		Alarm_Scroll.setViewportView(Alarm_Anounce_Tb);
+		Alarm_Anounce_Tb.setSurrendersFocusOnKeystroke(true);
+		Alarm_Anounce_Tb.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		Alarm_Anounce_Tb.setFont(Alarm_Anounce_Tb.getFont().deriveFont(Alarm_Anounce_Tb.getFont().getStyle() | Font.BOLD, Alarm_Anounce_Tb.getFont().getSize() + 2f));
+		Alarm_Anounce_Tb.setFillsViewportHeight(true);
+		Alarm_Anounce_Tb.setColumnSelectionAllowed(true);
+		Alarm_Anounce_Tb.setCellSelectionEnabled(true);
+		Alarm_Anounce_Tb.setBorder(UIManager.getBorder("Table.scrollPaneBorder"));
+	} 
 }
