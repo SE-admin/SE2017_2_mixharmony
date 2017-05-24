25652ae3990cdb09fa4586d521c88dbc0a859b53
201511035 <mira0809@naver.com>
2017-05-25 01:49:08 +0900

    #785 TODO_MANAGEMNET.java UI PUSH AFTER FEEDBACK

--- src/main/java/se/smu/TODO_MANAGEMNET.java
+++ src/main/java/se/smu/TODO_MANAGEMNET.java
@@ -12,10 +12,22 @@ import javax.swing.JScrollPane;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JComboBox;
+import javax.swing.border.TitledBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
 public class TODO_MANAGEMNET extends JFrame {
 
 	private JPanel contentPane;
+	private JTable Todo_Data_Tb;
 
 	
 	public static void main(String[] args) {
@@ -34,48 +46,68 @@ public class TODO_MANAGEMNET extends JFrame {
 
 	public TODO_MANAGEMNET() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 551, 566);
+		setBounds(100, 100, 763, 566);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(TODO_MANAGEMNET.class.getResource("/image/alarm--alarm-icon-91768.png")));
-		label.setBounds(449, 15, 50, 43);
-		contentPane.add(label);
+		JButton Add_Todo_Btn = new JButton("");
+		Add_Todo_Btn.setIcon(new ImageIcon(TODO_MANAGEMNET.class.getResource("/image/add.png")));
+		Add_Todo_Btn.setBounds(59, 73, 636, 54);
+		contentPane.add(Add_Todo_Btn);
 		
-		JButton button = new JButton("");
-		button.setIcon(new ImageIcon(TODO_MANAGEMNET.class.getResource("/image/add.png")));
-		button.setBounds(53, 93, 398, 54);
-		contentPane.add(button);
+		JButton Logout_Btn = new JButton("로그아웃");
+		Logout_Btn.setForeground(Color.WHITE);
+		Logout_Btn.setFont(Logout_Btn.getFont().deriveFont(Logout_Btn.getFont().getStyle() | Font.BOLD, Logout_Btn.getFont().getSize() + 4f));
+		Logout_Btn.setBackground(new Color(0, 0, 128));
+		Logout_Btn.setBounds(580, 440, 144, 42);
+		contentPane.add(Logout_Btn);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(506, 0, 23, 510);
-		contentPane.add(scrollPane);
+		JButton Subject_Btn = new JButton("수강 과목");
+		Subject_Btn.setForeground(Color.WHITE);
+		Subject_Btn.setFont(Subject_Btn.getFont().deriveFont(Subject_Btn.getFont().getStyle() | Font.BOLD, Subject_Btn.getFont().getSize() + 4f));
+		Subject_Btn.setBackground(new Color(0, 0, 128));
+		Subject_Btn.setBounds(287, 440, 144, 42);
+		contentPane.add(Subject_Btn);
 		
-		JButton button_1 = new JButton("정렬");
-		button_1.setFont(button_1.getFont().deriveFont(button_1.getFont().getStyle() | Font.BOLD, button_1.getFont().getSize() + 3f));
-		button_1.setForeground(Color.WHITE);
-		button_1.setBackground(new Color(0, 0, 128));
-		button_1.setBounds(17, 440, 102, 43);
-		contentPane.add(button_1);
+		JScrollPane Todo_Scroll = new JScrollPane();
+		Todo_Scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		Todo_Scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		Todo_Scroll.setBounds(31, 136, 688, 294);
+		contentPane.add(Todo_Scroll);
 		
-		JButton button_2 = new JButton("로그아웃");
-		button_2.setForeground(Color.WHITE);
-		button_2.setFont(button_2.getFont().deriveFont(button_2.getFont().getStyle() | Font.BOLD, button_2.getFont().getSize() + 4f));
-		button_2.setBackground(new Color(0, 0, 128));
-		button_2.setBounds(341, 441, 144, 42);
-		contentPane.add(button_2);
+		Todo_Data_Tb = new JTable();
+		Todo_Data_Tb.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"TODO명", "마감 기한", "실제 마감일", "중요도"
+			}
+		));
+		Todo_Data_Tb.getColumnModel().getColumn(0).setPreferredWidth(135);
+		Todo_Data_Tb.getColumnModel().getColumn(1).setPreferredWidth(135);
+		Todo_Data_Tb.getColumnModel().getColumn(2).setPreferredWidth(135);
+		Todo_Data_Tb.setSurrendersFocusOnKeystroke(true);
+		Todo_Data_Tb.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		Todo_Data_Tb.setFont(Todo_Data_Tb.getFont().deriveFont(Todo_Data_Tb.getFont().getStyle() | Font.BOLD, 18f));
+		Todo_Data_Tb.setFillsViewportHeight(true);
+		Todo_Data_Tb.setColumnSelectionAllowed(true);
+		Todo_Data_Tb.setCellSelectionEnabled(true);
+		Todo_Data_Tb.setBorder(UIManager.getBorder("Table.scrollPaneBorder"));
+		Todo_Scroll.setViewportView(Todo_Data_Tb);
 		
-		JButton button_3 = new JButton("수강 과목");
-		button_3.setForeground(Color.WHITE);
-		button_3.setFont(button_3.getFont().deriveFont(button_3.getFont().getStyle() | Font.BOLD, button_3.getFont().getSize() + 4f));
-		button_3.setBackground(new Color(0, 0, 128));
-		button_3.setBounds(164, 441, 144, 42);
-		contentPane.add(button_3);
+		JButton Importance_Btn = new JButton("");
+		Importance_Btn.setIcon(new ImageIcon(TODO_MANAGEMNET.class.getResource("/image/565112c0bf29a_thumb900.jpg")));
+		Importance_Btn.setBounds(674, 15, 50, 43);
+		contentPane.add(Importance_Btn);
+		
+		JComboBox Sort_Btn = new JComboBox();
+		Sort_Btn.setFont(Sort_Btn.getFont().deriveFont(Sort_Btn.getFont().getStyle() | Font.BOLD, Sort_Btn.getFont().getSize() + 4f));
+		Sort_Btn.setModel(new DefaultComboBoxModel(new String[] {"사전식순", "중요도순", "마감일순", "실제마감일순"}));
+		Sort_Btn.setForeground(new Color(255, 255, 255));
+		Sort_Btn.setBackground(new Color(0, 0, 128));
+		Sort_Btn.setBounds(17, 440, 144, 42);
+		contentPane.add(Sort_Btn);
 	}
-
 }
