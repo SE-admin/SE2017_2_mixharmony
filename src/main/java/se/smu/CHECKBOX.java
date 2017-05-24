89c077b895899991aba53e90d5b64d3ae3811c46
201511035 <mira0809@naver.com>
2017-05-25 01:10:28 +0900

    CHECKBOX.java ui push after feedback

--- src/main/java/se/smu/CHECKBOX.java
+++ src/main/java/se/smu/CHECKBOX.java
@@ -4,6 +4,7 @@ package se.smu;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
