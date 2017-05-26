f25d5193ebdefb7ce91d9e77d258dd0077d3d77b
201511035 <mira0809@naver.com>
2017-05-26 17:10:11 +0900

    #786 Todo_Management.java ui button modified

--- src/main/java/se/smu/Todo_Management.java
+++ src/main/java/se/smu/Todo_Management.java
@@ -45,36 +45,36 @@ public class Todo_Management extends JFrame {
 
 
 	public Todo_Management() {
+		setTitle("TO DO LIST");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 763, 566);
+		setBounds(100, 100, 763, 490);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		JButton Add_Todo_Btn = new JButton("");
-		Add_Todo_Btn.setIcon(new ImageIcon(Todo_Management.class.getResource("/image/add.png")));
-		Add_Todo_Btn.setBounds(59, 73, 636, 54);
-		contentPane.add(Add_Todo_Btn);
-		
 		JButton Logout_Btn = new JButton("로그아웃");
 		Logout_Btn.setForeground(Color.WHITE);
 		Logout_Btn.setFont(Logout_Btn.getFont().deriveFont(Logout_Btn.getFont().getStyle() | Font.BOLD, Logout_Btn.getFont().getSize() + 4f));
 		Logout_Btn.setBackground(new Color(0, 0, 128));
-		Logout_Btn.setBounds(580, 440, 144, 42);
+		Logout_Btn.setBounds(580, 381, 144, 42);
 		contentPane.add(Logout_Btn);
 		
 		JButton Subject_Btn = new JButton("수강 과목");
+		Subject_Btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		Subject_Btn.setForeground(Color.WHITE);
 		Subject_Btn.setFont(Subject_Btn.getFont().deriveFont(Subject_Btn.getFont().getStyle() | Font.BOLD, Subject_Btn.getFont().getSize() + 4f));
 		Subject_Btn.setBackground(new Color(0, 0, 128));
-		Subject_Btn.setBounds(287, 440, 144, 42);
+		Subject_Btn.setBounds(276, 381, 144, 42);
 		contentPane.add(Subject_Btn);
 		
 		JScrollPane Todo_Scroll = new JScrollPane();
 		Todo_Scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		Todo_Scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		Todo_Scroll.setBounds(31, 136, 688, 294);
+		Todo_Scroll.setBounds(28, 39, 688, 294);
 		contentPane.add(Todo_Scroll);
 		
 		Todo_Data_Tb = new JTable();
@@ -97,17 +97,20 @@ public class Todo_Management extends JFrame {
 		Todo_Data_Tb.setBorder(UIManager.getBorder("Table.scrollPaneBorder"));
 		Todo_Scroll.setViewportView(Todo_Data_Tb);
 		
-		JButton Importance_Btn = new JButton("");
-		Importance_Btn.setIcon(new ImageIcon(Todo_Management.class.getResource("/image/565112c0bf29a_thumb900.jpg")));
-		Importance_Btn.setBounds(674, 15, 50, 43);
-		contentPane.add(Importance_Btn);
+		JButton Add_Todo_Btn = new JButton("");
+		Todo_Scroll.setRowHeaderView(Add_Todo_Btn);
+		Add_Todo_Btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		Add_Todo_Btn.setIcon(new ImageIcon(Todo_Management.class.getResource("/image/add.png")));
 		
 		JComboBox Sort_Btn = new JComboBox();
 		Sort_Btn.setFont(Sort_Btn.getFont().deriveFont(Sort_Btn.getFont().getStyle() | Font.BOLD, Sort_Btn.getFont().getSize() + 4f));
 		Sort_Btn.setModel(new DefaultComboBoxModel(new String[] {"사전식순", "중요도순", "마감일순", "실제마감일순"}));
 		Sort_Btn.setForeground(new Color(255, 255, 255));
 		Sort_Btn.setBackground(new Color(0, 0, 128));
-		Sort_Btn.setBounds(17, 440, 144, 42);
+		Sort_Btn.setBounds(28, 381, 144, 42);
 		contentPane.add(Sort_Btn);
 	}
 }
