8bb48c77f9834ff10cdcdce09dcc26cfcda107f8
201511035 <mira0809@naver.com>
2017-05-29 16:03:30 +0900

    #791 Change_Subject.java ui modified

--- src/main/java/se/smu/Change_Subject.java
+++ src/main/java/se/smu/Change_Subject.java
@@ -2,6 +2,7 @@ package se.smu;
 
 
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -22,23 +23,32 @@ import javax.swing.border.EmptyBorder;
 public class Change_Subject extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField Subject_Tf;
+	private JLabel Subject_Tf;
 	private JTextField Subject_In;
-	private JTextField professor_Tf;
+	private JLabel professor_Tf;
 	private JTextField Professor_In;
-	private JTextField Dayofweek_Tf;
-	private JTextField Period_Tf;
+	private JLabel Dayofweek_Tf;
+	private JLabel Period_Tf;
 	private JTextField Dayofweek_In;
 	private JTextField Period_In;
-	private JTextField Year_Tf;
-	private JTextField Semester_Tf;
+	private JLabel Year_Tf;
+	private JLabel Semester_Tf;
 	private JTextField Year_In;
 	private JTextField Semester_In;
-	private JTextField Divclass_Tf;
+	private JLabel Divclass_Tf;
 	private JTextField Divclass_In;
-
+	private JPanel Panel_Subject;
 	//수정//
 	Subject_Dto getSubject_DtO;
+	private JLabel Professor_Lb;
+	private JLabel Dayofweek_Lb;
+	private JLabel Period_Lb;
+	private JPanel Panel_Year;
+	private JPanel Panel_Semester;
+	private JLabel Year_Lb;
+	private JLabel Semester_Lb;
+	private JLabel Divclass_Lb;
+	private JPanel Panel_Divclass;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -63,177 +73,211 @@ public class Change_Subject extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		panel.setBounds(0, 0, 529, 593);
-		contentPane.add(panel);
-		
 		JLabel Subject_Img = new JLabel("");
 		Subject_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/untitled.png")));
 		Subject_Img.setBounds(28, 15, 52, 41);
-		panel.add(Subject_Img);
+		contentPane.add(Subject_Img);
 		
-		Subject_Tf = new JTextField();
+		Panel_Subject = new JPanel();
+		Panel_Subject.setBackground(new Color(0, 0, 128));
+		Panel_Subject.setBounds(97, 15, 165, 41);
+		contentPane.add(Panel_Subject);
+		
+		Subject_Tf = new JLabel();
+		Panel_Subject.add(Subject_Tf);
 		Subject_Tf.setText("과목명");
 		Subject_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		Subject_Tf.setForeground(Color.WHITE);
 		Subject_Tf.setFont(Subject_Tf.getFont().deriveFont(Subject_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		Subject_Tf.setColumns(10);
 		Subject_Tf.setBackground(new Color(0, 0, 128));
-		Subject_Tf.setBounds(105, 15, 156, 46);
-		panel.add(Subject_Tf);
 		
 		Subject_In = new JTextField();
 		Subject_In.setText("");
 		Subject_In.setColumns(10);
 		Subject_In.setBounds(28, 76, 473, 41);
-		panel.add(Subject_In);
+		contentPane.add(Subject_In);
 		
 		JLabel Professor_Img = new JLabel("");
 		Professor_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/imagesASJC4VFF.jpg")));
 		Professor_Img.setBounds(28, 137, 52, 41);
-		panel.add(Professor_Img);
+		contentPane.add(Professor_Img);
 		
-		professor_Tf = new JTextField();
+		JPanel Panel_professor = new JPanel();
+		Panel_professor.setBackground(new Color(0, 0, 128));
+		Panel_professor.setBounds(97, 137, 165, 41);
+		contentPane.add(Panel_professor);
+		
+		professor_Tf = new JLabel();
+		Panel_professor.add(professor_Tf);
 		professor_Tf.setText("담당 교수");
 		professor_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		professor_Tf.setForeground(Color.WHITE);
 		professor_Tf.setFont(professor_Tf.getFont().deriveFont(professor_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		professor_Tf.setColumns(10);
 		professor_Tf.setBackground(new Color(0, 0, 128));
-		professor_Tf.setBounds(105, 132, 156, 46);
-		panel.add(professor_Tf);
 		
 		Professor_In = new JTextField();
-		Professor_In.setText("교수님");
-		Professor_In.setHorizontalAlignment(SwingConstants.RIGHT);
 		Professor_In.setFont(Professor_In.getFont().deriveFont(Professor_In.getFont().getStyle() | Font.BOLD, Professor_In.getFont().getSize() + 2f));
 		Professor_In.setColumns(10);
-		Professor_In.setBounds(28, 195, 473, 41);
-		panel.add(Professor_In);
+		Professor_In.setBounds(28, 195, 405, 41);
+		contentPane.add(Professor_In);
 		
 		JLabel Dayofweek_Img = new JLabel("");
 		Dayofweek_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/imagesL3LZX0G1.jpg")));
 		Dayofweek_Img.setBounds(28, 251, 52, 41);
-		panel.add(Dayofweek_Img);
+		contentPane.add(Dayofweek_Img);
 		
-		Dayofweek_Tf = new JTextField();
+		JPanel Panel_Dayofweek = new JPanel();
+		Panel_Dayofweek.setBackground(new Color(0, 0, 128));
+		Panel_Dayofweek.setBounds(97, 251, 165, 41);
+		contentPane.add(Panel_Dayofweek);
+		
+		Dayofweek_Tf = new JLabel();
+		Panel_Dayofweek.add(Dayofweek_Tf);
 		Dayofweek_Tf.setText("강의 요일");
 		Dayofweek_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		Dayofweek_Tf.setForeground(Color.WHITE);
 		Dayofweek_Tf.setFont(Dayofweek_Tf.getFont().deriveFont(Dayofweek_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		Dayofweek_Tf.setColumns(10);
 		Dayofweek_Tf.setBackground(new Color(0, 0, 128));
-		Dayofweek_Tf.setBounds(105, 246, 156, 46);
-		panel.add(Dayofweek_Tf);
 		
 		JLabel Period_Img = new JLabel("");
 		Period_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/imagesL3LZX0G1.jpg")));
 		Period_Img.setBounds(287, 251, 52, 41);
-		panel.add(Period_Img);
+		contentPane.add(Period_Img);
 		
-		Period_Tf = new JTextField();
+		JPanel Panel_Period = new JPanel();
+		Panel_Period.setBackground(new Color(0, 0, 128));
+		Panel_Period.setBounds(345, 251, 167, 41);
+		contentPane.add(Panel_Period);
+		
+		Period_Tf = new JLabel();
+		Panel_Period.add(Period_Tf);
 		Period_Tf.setText("강의 시간");
 		Period_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		Period_Tf.setForeground(Color.WHITE);
 		Period_Tf.setFont(Period_Tf.getFont().deriveFont(Period_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		Period_Tf.setColumns(10);
 		Period_Tf.setBackground(new Color(0, 0, 128));
-		Period_Tf.setBounds(345, 246, 156, 46);
-		panel.add(Period_Tf);
 		
 		Dayofweek_In = new JTextField();
-		Dayofweek_In.setText("요일");
 		Dayofweek_In.setHorizontalAlignment(SwingConstants.RIGHT);
 		Dayofweek_In.setFont(Dayofweek_In.getFont().deriveFont(Dayofweek_In.getFont().getStyle() | Font.BOLD, Dayofweek_In.getFont().getSize() + 2f));
 		Dayofweek_In.setColumns(10);
-		Dayofweek_In.setBounds(28, 307, 231, 41);
-		panel.add(Dayofweek_In);
+		Dayofweek_In.setBounds(28, 307, 182, 41);
+		contentPane.add(Dayofweek_In);
 		
 		Period_In = new JTextField();
-		Period_In.setText("교시");
-		Period_In.setHorizontalAlignment(SwingConstants.RIGHT);
 		Period_In.setFont(Period_In.getFont().deriveFont(Period_In.getFont().getStyle() | Font.BOLD, Period_In.getFont().getSize() + 2f));
 		Period_In.setColumns(10);
-		Period_In.setBounds(287, 307, 214, 41);
-		panel.add(Period_In);
+		Period_In.setBounds(287, 307, 173, 41);
+		contentPane.add(Period_In);
 		
 		JLabel Year_Img = new JLabel("");
 		Year_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/calendar-page-en.jpg")));
 		Year_Img.setBounds(28, 368, 52, 41);
-		panel.add(Year_Img);
+		contentPane.add(Year_Img);
 		
-		Year_Tf = new JTextField();
+		Panel_Year = new JPanel();
+		Panel_Year.setBackground(new Color(0, 0, 128));
+		Panel_Year.setBounds(97, 368, 165, 41);
+		contentPane.add(Panel_Year);
+		
+		Year_Tf = new JLabel();
+		Panel_Year.add(Year_Tf);
 		Year_Tf.setText("강의 년도");
 		Year_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		Year_Tf.setForeground(Color.WHITE);
 		Year_Tf.setFont(Year_Tf.getFont().deriveFont(Year_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		Year_Tf.setColumns(10);
 		Year_Tf.setBackground(new Color(0, 0, 128));
-		Year_Tf.setBounds(105, 363, 156, 46);
-		panel.add(Year_Tf);
 		
 		JLabel Semester_Img = new JLabel("");
 		Semester_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/calendar-page-en.jpg")));
 		Semester_Img.setBounds(287, 363, 52, 41);
-		panel.add(Semester_Img);
+		contentPane.add(Semester_Img);
 		
-		Semester_Tf = new JTextField();
+		Panel_Semester = new JPanel();
+		Panel_Semester.setBackground(new Color(0, 0, 128));
+		Panel_Semester.setBounds(347, 363, 165, 41);
+		contentPane.add(Panel_Semester);
+		
+		Semester_Tf = new JLabel();
+		Panel_Semester.add(Semester_Tf);
 		Semester_Tf.setText("강의 학기");
 		Semester_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		Semester_Tf.setForeground(Color.WHITE);
 		Semester_Tf.setFont(Semester_Tf.getFont().deriveFont(Semester_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		Semester_Tf.setColumns(10);
 		Semester_Tf.setBackground(new Color(0, 0, 128));
-		Semester_Tf.setBounds(345, 363, 156, 46);
-		panel.add(Semester_Tf);
 		
 		Year_In = new JTextField();
-		Year_In.setText("년도");
-		Year_In.setHorizontalAlignment(SwingConstants.RIGHT);
 		Year_In.setFont(Year_In.getFont().deriveFont(Year_In.getFont().getStyle() | Font.BOLD, Year_In.getFont().getSize() + 2f));
 		Year_In.setColumns(10);
-		Year_In.setBounds(28, 424, 231, 41);
-		panel.add(Year_In);
+		Year_In.setBounds(28, 424, 182, 41);
+		contentPane.add(Year_In);
 		
 		Semester_In = new JTextField();
-		Semester_In.setText("학기");
-		Semester_In.setHorizontalAlignment(SwingConstants.RIGHT);
 		Semester_In.setFont(Semester_In.getFont().deriveFont(Semester_In.getFont().getStyle() | Font.BOLD, Semester_In.getFont().getSize() + 2f));
 		Semester_In.setColumns(10);
-		Semester_In.setBounds(287, 424, 214, 41);
-		panel.add(Semester_In);
+		Semester_In.setBounds(287, 424, 173, 41);
+		contentPane.add(Semester_In);
 		
 		JLabel Divclass_Img = new JLabel("");
 		Divclass_Img.setIcon(new ImageIcon(Change_Subject.class.getResource("/image/imagesAN6AYLO3.jpg")));
 		Divclass_Img.setBounds(28, 480, 52, 41);
-		panel.add(Divclass_Img);
+		contentPane.add(Divclass_Img);
 		
-		Divclass_Tf = new JTextField();
+		Panel_Divclass = new JPanel();
+		Panel_Divclass.setBackground(new Color(0, 0, 128));
+		Panel_Divclass.setBounds(97, 480, 165, 41);
+		contentPane.add(Panel_Divclass);
+		
+		Divclass_Tf = new JLabel();
+		Panel_Divclass.add(Divclass_Tf);
 		Divclass_Tf.setText("분반");
 		Divclass_Tf.setHorizontalAlignment(SwingConstants.CENTER);
 		Divclass_Tf.setForeground(Color.WHITE);
 		Divclass_Tf.setFont(Divclass_Tf.getFont().deriveFont(Divclass_Tf.getFont().getStyle() | Font.BOLD, 21f));
-		Divclass_Tf.setColumns(10);
 		Divclass_Tf.setBackground(new Color(0, 0, 128));
-		Divclass_Tf.setBounds(105, 475, 156, 46);
-		panel.add(Divclass_Tf);
 		
 		Divclass_In = new JTextField();
-		Divclass_In.setText("분반");
-		Divclass_In.setHorizontalAlignment(SwingConstants.RIGHT);
 		Divclass_In.setFont(Divclass_In.getFont().deriveFont(Divclass_In.getFont().getStyle() | Font.BOLD, Divclass_In.getFont().getSize() + 2f));
 		Divclass_In.setColumns(10);
-		Divclass_In.setBounds(287, 480, 214, 41);
-		panel.add(Divclass_In);
+		Divclass_In.setBounds(287, 480, 173, 41);
+		contentPane.add(Divclass_In);
 		
 		JButton Check_Btn = new JButton("편집 등록");
 		Check_Btn.setForeground(Color.WHITE);
-		Check_Btn.setFont(Check_Btn.getFont().deriveFont(Check_Btn.getFont().getStyle() | Font.BOLD, Check_Btn.getFont().getSize() + 4f));
+		Check_Btn.setFont(Check_Btn.getFont().deriveFont(Check_Btn.getFont().getStyle() | Font.BOLD, 22f));
 		Check_Btn.setBackground(new Color(0, 0, 128));
 		Check_Btn.setBounds(357, 536, 144, 42);
-		panel.add(Check_Btn);
+		contentPane.add(Check_Btn);
+		
+		Professor_Lb = new JLabel("교수님");
+		Professor_Lb.setFont(Professor_Lb.getFont().deriveFont(Professor_Lb.getFont().getStyle() | Font.BOLD, 18f));
+		Professor_Lb.setBounds(445, 195, 67, 41);
+		contentPane.add(Professor_Lb);
+		
+		Dayofweek_Lb = new JLabel("요일");
+		Dayofweek_Lb.setFont(Dayofweek_Lb.getFont().deriveFont(Dayofweek_Lb.getFont().getStyle() | Font.BOLD, 18f));
+		Dayofweek_Lb.setBounds(212, 307, 67, 41);
+		contentPane.add(Dayofweek_Lb);
+		
+		Period_Lb = new JLabel("교시");
+		Period_Lb.setFont(Period_Lb.getFont().deriveFont(Period_Lb.getFont().getStyle() | Font.BOLD, 18f));
+		Period_Lb.setBounds(462, 307, 67, 41);
+		contentPane.add(Period_Lb);
+		
+		Year_Lb = new JLabel("년도");
+		Year_Lb.setFont(Year_Lb.getFont().deriveFont(Year_Lb.getFont().getStyle() | Font.BOLD, 18f));
+		Year_Lb.setBounds(212, 424, 67, 41);
+		contentPane.add(Year_Lb);
+		
+		Semester_Lb = new JLabel("학기");
+		Semester_Lb.setFont(Semester_Lb.getFont().deriveFont(Semester_Lb.getFont().getStyle() | Font.BOLD, 18f));
+		Semester_Lb.setBounds(462, 424, 67, 41);
+		contentPane.add(Semester_Lb);
+		
+		Divclass_Lb = new JLabel("분반");
+		Divclass_Lb.setFont(Divclass_Lb.getFont().deriveFont(Divclass_Lb.getFont().getStyle() | Font.BOLD, 18f));
+		Divclass_Lb.setBounds(462, 480, 67, 41);
+		contentPane.add(Divclass_Lb);
 		
 		//수정//
 		Check_Btn.addMouseListener(new MouseAdapter() {
