3fca18f2971fb7ffa056df6551b78431f520bd92
200911184 <strp1004@naver.com>
2017-06-06 16:16:02 +0900

    #792 정렬기능 오류 수정 Todo_Dao.java , Add_Todolist.java 내용수정

--- src/main/java/se/smu/Todo_Dao.java
+++ src/main/java/se/smu/Todo_Dao.java
@@ -488,11 +488,8 @@ public void userSelectAll3(DefaultTableModel model,int z) {
 			model.removeRow(0);  
 		}  
 		while (rs.next()) {  
-            Object data[] = { rs.getString(1), rs.getString(2), rs.getString(3), 
-            		rs.getString(4), rs.getString(5), rs.getString(6), 
-            		rs.getString(7), rs.getString(8), rs.getString(9), 
-            		rs.getString(10), rs.getString(11), rs.getString(12), 
-            		rs.getString(13), rs.getString(14), rs.getString(15)
+            Object data[] = { rs.getString(1), rs.getString(2), rs.getString(8), 
+            		rs.getString(18), rs.getString(15), rs.getString(17)
                     }; 
 			model.addRow(data);                  
 	}  
